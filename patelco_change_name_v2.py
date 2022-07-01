ee9eb62ecabe05895923d62a358066b062faef83
amitojbal <amitojbal@gmail.com>
2022-07-01 14:27:13 -0700

    file_change_patelco

---
+++ patelco_change_name_v2.py
@@ -0,0 +1,14 @@
+import os 
+from datetime import datetime as dt
+
+os.chdir(r'C:\Users\amitoj\Downloads\File_Name_Change\Patelco')
+
+for f in os.listdir():
+
+    f_txt_name, f_ext = os.path.splitext(f)
+
+    # 'format'.format(("Amex") + filename + {Month} + {ext})
+    new_name = '{}_{}_{}{}'.format(("Patelco"), ("2022-05-25"), ("May") , f_ext)
+    # print(new_name)
+
+    os.rename(f, new_name)
